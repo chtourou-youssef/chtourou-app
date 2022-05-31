2a1c23baeea58e361f329cb16c5f3b20f6eb5a8c
chtourou-youssef <ychtourou@spark-it.fr>
2022-05-31 12:39:01 +0200

    add files

---
+++ src/main/java/com/example/calculator/Calculator.java
@@ -0,0 +1,8 @@
+package com.example.calculator;
+import org.springframework.stereotype.Service;
+@Service
+public class Calculator {
+     public int sum(int a, int b) {
+          return a + b;
+     }
+}
